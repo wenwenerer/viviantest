48cfa5e354df22dfb49ca3f74c7adf63239a9691
wenwenerer <103995667+wenwenerer@users.noreply.github.com>
2024-08-02 01:29:31 +0800

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -31,14 +31,14 @@ for (let i = 1; i <= 99; i++) {
 }
 
 setTimeout(() => {
-    // 1. 在 pokemonList 中找到 ID 為 6 的寶可夢
+    // 1. 列出 id 為 6 的寶可夢名稱（name）
 let pokemon6 = pokemonList.find(p => p.id === 6);
 if (pokemon6) {
     console.log(`1. ID6 的寶可夢名稱為: ${pokemon6.name}`);
 } else {
     console.log("1. 無ID6 的寶可夢");
 }
-    // 2. 列出 id < 20, id > 0 的寶可夢名稱及屬性，按 id 升序排序
+    // 2. 列出 id < 20, id > 0 的寶可夢名稱（name）以及其寶可夢的屬性（types），依照 id 由小至大排序
     console.log("\n2. ID 在 1 ~ 19 之間的寶可夢名稱及屬性（按 ID 小到大）:");
     pokemonList.filter(p => p.id > 0 && p.id < 20)
                .sort((a, b) => a.id - b.id)
@@ -46,7 +46,7 @@ if (pokemon6) {
                 console.log(`ID: ${p.id}, 名稱: ${p.name}, 屬性: ${p.types.join(', ')}`);
                });
 
-    // 3. 列出 id < 100, id > 0 且體重 < 50 的寶可夢名稱及體重，按體重降序排序
+    // 3. 列出 id < 100, id > 0 的寶可夢中，體重（weight） < 50 的寶可夢名稱（name）及寶可夢體重（weight），並且依照體重由大至小排序
     console.log("\n3. ID 在 1 ~ 99 之間且體重小於 50 的寶可夢（大到小）:");
     pokemonList.filter(p => p.id > 0 && p.id < 100 && p.weight < 50)
                .sort((a, b) => b.weight - a.weight)
